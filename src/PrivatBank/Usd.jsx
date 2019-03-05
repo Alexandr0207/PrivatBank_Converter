16bb770cbc4e378ade159077bf143ff8ea5ccad5
Alexandr Zhuk <sanek201dg@ukr.net>
2019-03-05 15:55:55 +0200

    USDcreateComponentFixed

--- src/PrivatBank/Usd.jsx
+++ src/PrivatBank/Usd.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Usd = ({inputUS, inputChange, sumUS}) => {
+const Usd = ({inputUS, inputChangeUSD, sumUS}) => {
   return (
     <div>
     <p className="title_converter">USD in UAH</p>
     <form onSubmit={(e) => e.preventDefault()} className="usd">
-    <input type="number" className="inputUsd" value={inputUS} placeholder="Введите сумму USD" onChange={inputChange}/>
+    <input type="number" className="inputUsd" value={inputUS} placeholder="Введите сумму USD" onChange={inputChangeUSD}/>
     <span className="sym">{`${(Number(sumUS).toFixed(2))} грн`}</span>
     </form>
   </div>
